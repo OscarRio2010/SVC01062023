9d03bd6f56dd0b48167307f8c394e659239280af
OscarRio2010 <leanhtuyenvta@gmail.com>
2024-06-05 15:44:56 +0700

    Done b4

--- fe/sivishop/src/pages/users/theme/header/index.js
+++ fe/sivishop/src/pages/users/theme/header/index.js
@@ -1,62 +1,138 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import "./style.scss";
 import {
   AiOutlineFacebook,
   AiOutlineInstagram,
   AiOutlineLinkedin,
   AiOutlineMail,
+  AiOutlineShoppingCart,
   AiOutlineTwitter,
   AiOutlineUser,
 } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { formatter } from "utils/fomater";
+import { ROUTERS } from "utils/router";
 const Header = () => {
+  const [menus, setMenus] = useState([
+    {
+      name: "Home",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "Shop",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "Product",
+      path: ROUTERS.USER.HOME,
+      isShowSubmenu: false,
+      child: [
+        {
+          name: "Meat",
+          path: "",
+        },
+        {
+          name: "Vegetable",
+          path: "",
+        },
+        {
+          name: "Fast Food",
+          path: "",
+        },
+      ],
+    },
+    {
+      name: "Posts",
+      path: ROUTERS.USER.HOME,
+    },
+    {
+      name: "Contact",
+      path: ROUTERS.USER.HOME,
+    },
+  ]);
+
   return (
-    <div className="header__top">
-      <div className="container">
-        <div className="row">
-          <div className="col-6 header__top_left">
-            <ul>
-              <li>
-                <AiOutlineMail />
-                leanhtuyenvta@gmai.com
-              </li>
-              <li>Free shipping for orders from {formatter(300)} </li>
-            </ul>
-          </div>
-          <div className="col-6 header__top_right">
-            <ul>
-              <li>
-                <Link to={""}>
-                  <AiOutlineFacebook />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiOutlineInstagram />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiOutlineLinkedin />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiOutlineTwitter />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <AiOutlineUser />
-                  <span>Login</span>
-                </Link>
-              </li>
-            </ul>
+    <>
+      <div className="header__top">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 header__top_left">
+              <ul>
+                <li>
+                  <AiOutlineMail />
+                  leanhtuyenvta@gmai.com
+                </li>
+                <li>Free shipping for orders from {formatter(300)} </li>
+              </ul>
+            </div>
+            <div className="col-6 header__top_right">
+              <ul>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineFacebook />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineInstagram />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineLinkedin />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineTwitter />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <AiOutlineUser />
+                    <span>Login</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <div className="header__logo">
+              <h1>SiVi SHOP</h1>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <nav className="header__menu">
+              <ul>
+                {menus?.map((menu, menuKey) => (
+                  <li key={menuKey} className={menuKey === 0 ? "active" : ""}>
+                    <Link to={menu?.path}>{menu?.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+          <div className="col-lg-3">
+            <div className="header__cart">
+              <div className="header__cart_price">
+                <span>{formatter(20000000)}</span>
+              </div>
+              <ul>
+                <li>
+                  <Link to="#">
+                    <AiOutlineShoppingCart /> <span>5</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
